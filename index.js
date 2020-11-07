4dec6e633f062909695746a348727e922ec7cc4c
Shubham Gupta <gupta.shubham6262@gmail.com>
2020-11-08 03:51:42 +0530

    initial push to heroku

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const photosRouter = require("./routers/photos");
+require("dotenv").config();
+require("./db/connection");
+
+const app = express();
+const PORT = process.env.PORT || 3300;
+
+app.use(cors());
+app.use(photosRouter);
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`);
+});
